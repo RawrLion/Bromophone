5aedb8be0f110405e08770256fae31a9bef6c220
maxploded <pro100Giggles@gmail.com>
2017-06-03 23:09:33 +0300

    Buttons are fine Server in on

--- app/src/main/java/com/borstsch/bromophone/MainActivity.java
+++ app/src/main/java/com/borstsch/bromophone/MainActivity.java
@@ -1,20 +1,22 @@
 package com.borstsch.bromophone;
 
-import android.net.wifi.WifiManager;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.format.Formatter;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 import com.borstsch.bromophone.musicplayer.PlayerActivity;
 
@@ -41,6 +43,86 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PlayerActivity.class);
         startActivity(intent);
     }
+
+    // ---------------- START OF MY SHIT ---------------------------
+
+    private ServerSocket mServerSocket;
+    private int mLocalPort;
+    private NsdManager mNsdManager;
+    private NsdManager.RegistrationListener mRegistrationListener;
+    private String mServiceName;
+
+    public void registerService(int port) {
+        // Create the NsdServiceInfo object, and populate it.
+        NsdServiceInfo serviceInfo  = new NsdServiceInfo();
+
+        // The name is subject to change based on conflicts
+        // with other services advertised on the same network.
+        serviceInfo.setServiceName("NsdSound");
+        serviceInfo.setServiceType("_http._tcp.");
+        serviceInfo.setPort(port);
+
+        mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+
+        mNsdManager.registerService(
+                serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
+    }
+
+    public void initializeServerSocket() throws IOException {
+        // Initialize a server socket on the next available port.
+        mServerSocket = new ServerSocket(0);
+
+        // Store the chosen port.
+        mLocalPort =  mServerSocket.getLocalPort();
+    }
+
+    public void onServerClick(View view) throws IOException {
+        initializeServerSocket();
+        initializeRegistrationListener();
+        registerService(mLocalPort);
+        textView.setText("ZHOPA");
+    }
+
+
+
+    public void initializeRegistrationListener() {
+        mRegistrationListener = new NsdManager.RegistrationListener() {
+
+            @Override
+            public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
+                // Save the service name.  Android may have changed it in order to
+                // resolve a conflict, so update the name you initially requested
+                // with the name Android actually used.
+                mServiceName = NsdServiceInfo.getServiceName();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView = (TextView) findViewById(R.id.ip_text);
+                        textView.setText("Success, port: " + mLocalPort + ", Service name: " + mServiceName);
+                    }
+                });
+            }
+
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Registration failed!  Put debugging code here to determine why.
+            }
+
+            @Override
+            public void onServiceUnregistered(NsdServiceInfo arg0) {
+                // Service has been unregistered.  This only happens when you call
+                // NsdManager.unregisterService() and pass in this listener.
+            }
+
+            @Override
+            public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Unregistration failed.  Put debugging code here to determine why.
+            }
+        };
+    }
+
+
+    // --------------- END OF MY SHIT ---------------------
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
