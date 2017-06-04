b5e76f2e77c616859c2f39f9d45751e371334661
maxploded <pro100Giggles@gmail.com>
2017-06-04 16:32:02 +0300

    IP List and server-client handshake

--- app/src/main/java/com/borstsch/bromophone/Dispatcher.java
+++ app/src/main/java/com/borstsch/bromophone/Dispatcher.java
@@ -4,41 +4,60 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.text.format.Formatter;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
-
-/**
- * Created by bugogish on 04.06.17.
- */
 
 public class Dispatcher {
 
     private ServerSocket mServerSocket;
-    private int mLocalPort;
+    private Integer mLocalPort = null;
     private NsdManager mNsdManager;
     private NsdManager.RegistrationListener mRegistrationListener;
     private String mServiceName;
     private Context mContext;
 
     private NsdManager.DiscoveryListener mDiscoveryListener;
-    private NsdManager.ResolveListener mResolveListener;
+    //private NsdManager.ResolveListener mResolveListener;
 
     private NsdServiceInfo mService;
+    private int hostPort;
+    private InetAddress hostAddress;
+
+    private static final String REQUEST_CONNECT_CLIENT = "request-connect-client";
+
+    private List<String> clientIPs;
+    SocketServerThread socketServerThread;
 
     public Dispatcher(Context mContext) {
         this.mContext = mContext;
     }
 
     public void runServer() throws IOException {
-        initializeServerSocket();
+        clientIPs = new ArrayList<>();
+        socketServerThread = new SocketServerThread();
+        socketServerThread.start();
         initializeRegistrationListener();
+        while (mLocalPort == null) {
+            continue;
+        }
         registerService(mLocalPort);
     }
 
@@ -65,12 +84,86 @@ public class Dispatcher {
                 serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
     }
 
-    public void initializeServerSocket() throws IOException {
-        // Initialize a server socket on the next available port.
-        mServerSocket = new ServerSocket(0);
+    private class SocketServerThread extends Thread {
 
-        // Store the chosen port.
-        mLocalPort =  mServerSocket.getLocalPort();
+        @Override
+        public void run() {
+
+            Socket socket = null;
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+
+            try {
+                Log.i(TAG, "Creating server socket");
+                mServerSocket = new ServerSocket(0);
+                mLocalPort =  mServerSocket.getLocalPort();
+
+                while (true) {
+                    socket = mServerSocket.accept();
+                    dataInputStream = new DataInputStream(
+                            socket.getInputStream());
+                    dataOutputStream = new DataOutputStream(
+                            socket.getOutputStream());
+
+                    String messageFromClient, messageToClient, request;
+
+                    //If no message sent from client, this code will block the program
+                    messageFromClient = dataInputStream.readUTF();
+
+                    final JSONObject jsondata;
+                    jsondata = new JSONObject(messageFromClient);
+
+                    try {
+                        request = jsondata.getString("request");
+
+                        if (request.equals(REQUEST_CONNECT_CLIENT)) {
+                            String clientIPAddress = jsondata.getString("ipAddress");
+
+                            // Add client IP to a list
+                            clientIPs.add(clientIPAddress);
+                            messageToClient = "Connection Accepted";
+                            dataOutputStream.writeUTF(messageToClient);
+                        } else {
+                            // There might be other queries, but as of now nothing.
+                            dataOutputStream.flush();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Unable to get request");
+                        dataOutputStream.flush();
+                    }
+                }
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
     }
 
     public void initializeRegistrationListener() {
@@ -177,17 +270,124 @@ public class Dispatcher {
                 return;
             }
             mService = serviceInfo;
-            //int port = mService.getPort();
-            //InetAddress host = mService.getHost();
+            hostPort = mService.getPort();
+            hostAddress = mService.getHost();
 
-            ((Activity) mContext).runOnUiThread(new Runnable() {
+            connectToHost();
+
+            /*((Activity) mContext).runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     TextView textView = (TextView) ((Activity) mContext).findViewById(R.id.ip_text);
                     textView.setText("Client ok, Service name: " + mService.getServiceName()
                             + "port: " + mService.getPort() + " host: " + mService.getHost());
                 }
-            });
+            });*/
+        }
+    }
+
+    private void connectToHost() {
+
+        if (hostAddress == null) {
+            Log.e(TAG, "Host Address is null");
+            return;
+        }
+
+        String ipAddress = getLocalIpAddress();
+        JSONObject jsonData = new JSONObject();
+
+        try {
+            jsonData.put("request", REQUEST_CONNECT_CLIENT);
+            jsonData.put("ipAddress", ipAddress);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e(TAG, "can't put request");
+            return;
+        }
+
+        new SocketServerTask().execute(jsonData);
+    }
+
+    private String getLocalIpAddress() {
+        WifiManager wm = (WifiManager) mContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        return ip;
+    }
+
+    private class SocketServerTask extends AsyncTask<JSONObject, Void, Void> {
+        private JSONObject jsonData;
+        private boolean success;
+
+        @Override
+        protected Void doInBackground(JSONObject... params) {
+            Socket socket = null;
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+            jsonData = params[0];
+
+            try {
+                // Create a new Socket instance and connect to host
+                socket = new Socket(hostAddress, hostPort);
+
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
+                // transfer JSONObject as String to the server
+                dataOutputStream.writeUTF(jsonData.toString());
+                Log.i(TAG, "waiting for response from host");
+
+                // Thread will wait till server replies
+                String response = dataInputStream.readUTF();
+                if (response.equals("Connection Accepted")) {
+                    success = true;
+                } else {
+                    success = false;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                success = false;
+            } finally {
+
+                // close socket
+                if (socket != null) {
+                    try {
+                        Log.i(TAG, "closing the socket");
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // close input stream
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // close output stream
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            TextView textView = (TextView) ((Activity) mContext).findViewById(R.id.ip_text);
+            if (success) {
+                textView.setText("Connected");
+            } else {
+                textView.setText("NOT Connected");
+            }
         }
     }
 }
